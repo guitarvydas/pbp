55212be5bc685c0db1c8cdf788c41cd7a8ff3817
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-06-27 16:20:04 -0400

    repl.py

--- @setup-tools.sh
+++ @setup-tools.sh
@@ -2,9 +2,11 @@
 # git clone https://github.com/guitarvydas/pbp
 ./pbp/@setup-init.sh
 cp ./pbp/kernel/kernel0d.py ./pbp
+cp ./pbp/kernel/repl.py ./pbp
 rm -rf ./pbp/kernel
 mkdir ./pbp/kernel
 mv ./pbp/kernel0d.py ./pbp/kernel
+mv ./pbp/repl.py ./pbp/kernel
 
 cp ./pbp/@make-proto/@make .
 cp ./pbp/@make-proto/@defc .
