263edac8db5f90ea2fb323fe5b45b8fef541927e
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-09-08 02:19:52 -0400

    created _/

--- kernel/regression_test_tas.bash
+++ kernel/regression_test_tas.bash
@@ -2,7 +2,7 @@
 set -e
 python3 regression_test_main.py . - "$1.rt" main kernel.drawio.json | node decodeoutput.mjs
 if [ -f "out.md" ]; then cat "out.md"; fi
-mv out.py "_r_python/$1.py"
-mv out.js "_r_js/$1.js"
-mv out.lisp "_r_lisp/$1.lisp"
-mv out.c "_r_c/$1.c"
+mv out.py "_/_r_python/$1.py"
+mv out.js "_/_r_js/$1.js"
+mv out.lisp "_/_r_lisp/$1.lisp"
+mv out.c "_/_r_c/$1.c"
--- kernel/tas.bash
+++ kernel/tas.bash
@@ -3,7 +3,7 @@ set -e
 python3 main.py . - "$1.rt" main kernel.drawio.json | node decodeoutput.mjs
 if [ -f "out.✗" ]; then cat "out.✗"; exit 1; fi
 if [ -f "out.md" ]; then cat "out.md"; fi
-mv out.py _python/$1.py
-mv out.js _js/$1.js
-mv out.lisp _lisp/$1.lisp
-mv out.c _c/$1.c
+mv out.py _/_python/$1.py
+mv out.js _/_js/$1.js
+mv out.lisp _/_lisp/$1.lisp
+mv out.c _/_c/$1.c
