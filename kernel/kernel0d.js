090bb11394466cb5a7b4c737e084cacad342c8f7
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-09-10 17:21:08 -0400

    gensym.rt

--- kernel/kernel0d.js
+++ kernel/kernel0d.js
@@ -1,3 +1,18 @@
+let  digits = [ "₀", "₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉", "₁₀", "₁₁", "₁₂", "₁₃", "₁₄", "₁₅", "₁₆", "₁₇", "₁₈", "₁₉", "₂₀", "₂₁", "₂₂", "₂₃", "₂₄", "₂₅", "₂₆", "₂₇", "₂₈", "₂₉"];/* line 7 *//* line 8 *//* line 9 */
+function subscripted_digit (n) {                       /* line 10 *//* line 11 */
+    if (((( n >=  0) && ( n <=  29)))) {               /* line 12 */
+      return  digits [ n];                             /* line 13 */
+    }
+    else {                                             /* line 14 */
+      return  ( "₊".toString ()+ `${ n}`.toString ())  /* line 15 */;/* line 16 */
+    }                                                  /* line 17 *//* line 18 */
+}
+
+function gensymbol (s) {                               /* line 19 *//* line 20 */
+    let name_with_id =  ( s.toString ()+ subscripted_digit ( counter).toString ()) /* line 21 */;
+    counter =  counter+ 1;                             /* line 22 */
+    return  name_with_id;                              /* line 23 *//* line 24 *//* line 25 */
+}
 import * as fs from 'fs';
 import path from 'path';
 import execSync from 'child_process';
